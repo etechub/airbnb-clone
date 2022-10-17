346f0b3b64fba8f223c8363bd0098cb10f0ad809
Yhammzy <yisaahmed04@gmail.com>
2022-10-17 12:58:01 +0100

    jjhguh

---
+++ main.js
@@ -0,0 +1,13 @@
+// $(document).ready(function () {
+//     $(".owl-carousel").owlCarousel({
+//       items:1,
+//       loop:true,
+//       nav:true,
+//       dots:true,
+//       autoplay:true,
+//       autoplayTimeout: 3000,
+//       autoplaySpeed: 1000,
+//       smartSpeed: 1500,
+//       autoplayHoverPause:true
+//     });
+//   });
