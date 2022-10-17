d9e7b135128e54330216a7434f3e6f452893daa1
Yhammzy <yisaahmed04@gmail.com>
2022-10-17 21:03:02 +0100

    dtdcgh

--- main.js
+++ main.js
@@ -11,3 +11,29 @@
 //       autoplayHoverPause:true
 //     });
 //   });
+var button = document.querySelector('.fa-bars')
+var list = document.querySelector('.menu')
+var icon = document.querySelector('.fa-heart')
+
+var count = 0
+
+button.addEventListener('click', function() {
+    if (count == 0 ) {
+        list.style.display = 'flex'
+        count = 1
+        return;
+    }
+    list.style.display = 'none'
+    count = 0
+    return
+})
+icon.addEventListener('click', function() {
+    if (count == 0 ) {
+        icon.style.color = 'red'
+        count = 1
+        return;
+    }
+    icon.style.color = 'rgba(0, 0, 0, 0.7)'
+    count = 0
+    return
+})
